eaf982023da0d53eda156510aa5c796a8eb0447b
JKnezovic <jo.knezovic@gamil.com>
2024-03-09 16:59:42 +0100

    support aws s3 client side routing

--- frontend/jozo-ide-gas/src/index.js
+++ frontend/jozo-ide-gas/src/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { createBrowserHistory } from "history";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const history = createBrowserHistory();
+
+// Remove the '#' from URLs if it exists
+const path = (/#!(\/.*)$/.exec(window.location.hash) || [])[1];
+if (path) {
+  history.replace(path);
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
