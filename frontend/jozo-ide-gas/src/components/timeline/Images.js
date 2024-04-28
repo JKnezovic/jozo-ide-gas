f932a2e124532074f8f05547f4817a5311030ba4
JKnezovic <jo.knezovic@gamil.com>
2024-04-28 18:27:41 +0200

    added low quality image placeholder for thumbnails

--- frontend/jozo-ide-gas/src/components/timeline/Images.js
+++ frontend/jozo-ide-gas/src/components/timeline/Images.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { PhotoProvider, PhotoView } from "react-photo-view";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const CustomOverlay = (props) => {
   return <div className="custom-overlay">{props.images[props.index]?.desc}</div>;
@@ -13,7 +14,7 @@ const Images = ({ images }) => {
           <PhotoView key={index} src={item.src} desc={item.desc}>
             {index < 3 ? (
               <div style={{ position: "relative", display: "inline-block" }}>
-                <img className="thumb" src={item.src} alt="" loading="lazy" />
+                <LazyLoadImage className="thumb" src={item.lqipsrc} alt="" />
                 {index === 2 && images.length > 3 && (
                   <div className="thumb-overlay">{`+${images.length - 3}`}</div>
                 )}
